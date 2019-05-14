fb578e6b08e06838062cad5a625845feb2469766
Flooorent <florent.moiny@gmail.com>
2019-05-14 18:55:27 +0200

    started filling tests

--- test/2_functional-tests.js
+++ test/2_functional-tests.js
@@ -6,14 +6,13 @@
 *       (if additional are added, keep them at the very end!)
 */
 
-var chaiHttp = require('chai-http');
-var chai = require('chai');
-var assert = chai.assert;
-var server = require('../server');
+const chaiHttp = require('chai-http');
+const chai = require('chai');
+const server = require('../server');
 
 const { client: mongoClient } = require('../config/mongo')
 
-chai.use(chaiHttp);
+chai.use(chaiHttp).should();
 
 describe('Functional Tests', function() {
 
@@ -43,23 +42,59 @@ describe('Functional Tests', function() {
   
   describe('POST /api/issues/{project} => object with issue data', function() {
       it('should fill in every field', function(done) {
-       chai.request(server)
-        .post('/api/issues/test')
-        .send({
-          issue_title: 'Title',
-          issue_text: 'text',
-          created_by: 'Functional Test - Every field filled in',
-          assigned_to: 'Chai and Mocha',
-          status_text: 'In QA'
-        })
-        .end(function(err, res){
-          assert.equal(res.status, 200);
-          
-          //fill me in too!
-          
-          done();
+        const issueTitle = 'Title'
+        const issueText = 'text'
+        const createdBy = 'Functional Test - Every field filled in'
+        const assignedTo = 'Chai and Mocha'
+        const statusText = 'In QA'
+
+        const beforeCall = new Date()
+        
+        chai.request(server)
+          .post('/api/issues/test')
+          .send({
+            issue_title: issueTitle,
+            issue_text: issueText,
+            created_by: createdBy,
+            assigned_to: assignedTo,
+            status_text: statusText
+          })
+          .end(function(err, res){
+            const afterCall = new Date()
+
+            res.should.have.status(200);
+            res.should.be.json
+            res.body.should.be.an('object')
+            res.body.should.have.property('issue_title')
+            res.body.should.have.property('issue_text')
+            res.body.should.have.property('created_by')
+            res.body.should.have.property('assigned_to')
+            res.body.should.have.property('status_text')
+            res.body.should.have.property('created_on')
+            res.body.should.have.property('updated_on')
+            res.body.should.have.property('open')
+            res.body.should.have.property('_id')
+            res.body.issue_title.should.equal(issueTitle)
+            res.body.issue_text.should.equal(issueText)
+            res.body.created_by.should.equal(createdBy)
+            res.body.assigned_to.should.equal(assignedTo)
+            res.body.status_text.should.equal(statusText)
+            
+            res.body.created_on.should.be.above(beforeCall)
+            res.body.created_on.should.be.below(afterCall)
+            
+            // TODO: finish assertions
+            
+            done();
+          });
         });
-      });
+
+      /*
+      created_on(date/time)
+      updated_on(date/time)
+      open(boolean, true for open, false for closed)
+      _id.
+      */
       
       it('Required fields filled in', function(done) {
         
@@ -94,17 +129,17 @@ describe('Functional Tests', function() {
         .get('/api/issues/test')
         .query({})
         .end(function(err, res){
-          assert.equal(res.status, 200);
-          assert.isArray(res.body);
-          assert.property(res.body[0], 'issue_title');
-          assert.property(res.body[0], 'issue_text');
-          assert.property(res.body[0], 'created_on');
-          assert.property(res.body[0], 'updated_on');
-          assert.property(res.body[0], 'created_by');
-          assert.property(res.body[0], 'assigned_to');
-          assert.property(res.body[0], 'open');
-          assert.property(res.body[0], 'status_text');
-          assert.property(res.body[0], '_id');
+          res.should.have.status(200);
+          res.body.should.be.an('array')
+          res.body[0].should.have.property('issue_title')
+          res.body[0].should.have.property('issue_text')
+          res.body[0].should.have.property('created_on')
+          res.body[0].should.have.property('updated_on')
+          res.body[0].should.have.property('created_by')
+          res.body[0].should.have.property('assigned_to')
+          res.body[0].should.have.property('open')
+          res.body[0].should.have.property('status_text')
+          res.body[0].should.have.property('_id')
           done();
         });
       });
